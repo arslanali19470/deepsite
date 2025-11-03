b71658680f92106c38e942bcfaad9e301deeac2e
thestackpilot <dev-ops@automaxion.io>
2025-11-03 17:25:02 +0500

    add margin

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body style={{ background: "white", marginTop: 50 }}
+      <body style={{ background: "white", marginTop: 50, margin: 20 }}
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
